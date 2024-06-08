b385aa8fd7e49d8901830330c077b7a78962fc12
christianldev <christlopbsc12@gmail.com>
2024-06-08 12:06:06 -0500

    feat: :zap: Implementacion de mailtrap para verificacion email

--- TodoListApp/backend/config/mail.php
+++ TodoListApp/backend/config/mail.php
@@ -57,6 +57,16 @@ return [
             // ],
         ],
 
+         // start mailtrap transport
+         'mailtrap' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+        ],
+
         'postmark' => [
             'transport' => 'postmark',
             // 'client' => [
